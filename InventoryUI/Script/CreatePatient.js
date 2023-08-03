8f32c6a20884b4734c33379cf68bfb924ade7ab3
rutujapatil2023 <rutuja.patil@encora.com>
2023-08-03 12:33:59 +0530

    Add code for create parient and get patient end points. Add code for create Rx and get Rx

---
+++ InventoryUI/Script/CreatePatient.js
@@ -0,0 +1,68 @@
+﻿  var requestJsonBody;
+  var aqHttpRequest;
+  var responseBody;
+  var patientId;
+  var aqHttpResponse;
+  var requestBody;
+  var fhirId;
+  var firstName;
+  var lastName;
+  var requestUrl;
+  var jsonResponseBody;
+  
+  //Generate dynamic data for create petient request body
+ function generateDynamicData(){
+  patientId ="PID" +Math.floor(100000 + Math.random() * 900000);
+  firstName ="Raj" +Math.floor(100000 + Math.random() * 900000);
+  lastName ="Patil" +Math.floor(100000 + Math.random() * 900000);
+  fhirId ="FHR" +Math.floor(100000 + Math.random() * 900000);
+  Log.Message("generateDynamicData Patient Id: "+patientId+" First Name: "+firstName+" Last Name: "+lastName);
+ }
+ 
+  //Create New Patient
+  function createPatient(){
+  requestUrl= Project.Variables.patientBaseUrl + "SavePatient";
+  requestBody = '{"address":{"street":"127 Green Apple Road","city":"Sacramento","state":"California","country":"US","postalCode":"00812"},"dateOfBirth":"1959-02-03","email":"email@testing.com","fhirId":"'+fhirId+'","language":"en","name":{"title":"Mr.","firstName":"'+firstName+'","middleName":"M.","lastName":"'+lastName+'","suffix":"II"},"patientId":"'+patientId+'","idType":"EPI","phone":"916-812-6641","privacyConsent":{"active":true,"start":"2021-12-01T19:25:32.602Z","end":null},"sex":"Male","fyis":[{"comment":"A very nice person","typeId":"41000058","typeName":"Drug-Seeking Behavior"}]}'
+  Log.Message("createPatient RequestBodyData:"+ requestBody);
+  
+  // Create the aqHttpRequest object
+   aqHttpRequest = aqHttp.CreateRequest("PUT", requestUrl);
+  
+  // Specify the Content-Type header value
+  aqHttpRequest.SetHeader("Content-Type", "application/json");
+ 
+  // Send the request, create the aqHttpResponse object
+  responseBody = aqHttpRequest.Send(requestBody);
+  Log.Message("createPatient status code:"+responseBody.StatusCode);
+  verifyStatusCode(responseBody, 201);  
+ }
+
+//Get newly created patient
+  function getPatient(){
+   const requestUrl = Project.Variables.patientBaseUrl+patientId;
+   aqHttpRequest = aqHttp.CreateGetRequest(requestUrl);
+   //Send the request, get an aqHttpResponse object
+   responseBody = aqHttpRequest.Send();
+   Log.Message("getPatient response is: "+responseBody);
+   verifyStatusCode(responseBody, 200); 
+   storePatientDetails();
+  }
+
+  //Store required patient details
+  function storePatientDetails(){
+    jsonResponseBody = JSON.parse(responseBody);
+    patientId= jsonResponseBody.patientId;
+    Log.Message("PatinetID from response: "+patientId);
+    Project.Variables.patientID= patientId;
+    Log.Message(Project.Variables.patientID);
+  }
+
+  //Verify status code
+ function verifyStatusCode(responseBody, expectedStatusCode){
+  if(responseBody.StatusCode==expectedStatusCode){
+    Log.Message("verifyStatusCode status code is: "+responseBody.StatusCode);
+  }else{
+    Log.Error("verifyStatusCode Status code is incorrect. Expected status code is: "+expectedStatusCode)
+  }
+ }
+module.exports.verifyStatusCode = verifyStatusCode;
---
+++ InventoryUI/Script/CreateRxForPatient.js
@@ -0,0 +1,59 @@
+﻿  var createPatient = require("CreatePatient");
+  var aqHttpRequest;
+  var responseBody;
+  var aqHttpResponse;
+  var requestBody;
+  var requestUrl;
+  var jsonResponseBody;
+  var rxId;
+  var rxNumber;
+  var patientId = Project.Variables.patientID;
+ 
+  //Create New Patient
+  function createRx(){
+  rxId ="rxId" +Math.floor(100000 + Math.random() * 900000);
+  rxNumber="rxNumber" +Math.floor(100000 + Math.random() * 900000);
+  
+  //Request url for create rx.
+  requestUrl= Project.Variables.rxBaseUrl + "putRx";
+  
+  //Request body for create rx.
+  requestBody = '{"prescriptionId":"'+rxId+'","patientId":"'+patientId+'","prescriptionNumber":"'+rxNumber+'","status":"Active","prescriber":{"prescriberId":"Prescriber-123","name":{"title":"Dr.","firstName":"M.","middleName":"Montgomery","lastName":"Burns","suffix":"Jr."},"address":{"street":"123 Anystreet, top floor","city":"Toronto","state":"Ontario","country":"Canada","postalCode":"M8C 1F6"},"phone":"555 123-4567"},"pharmacy":{"storeId":"7354983","name":"Pharmacy Name","phone":"1 (800) 555-1234","address":{"street":"123 Anystreet, top floor","city":"Toronto","state":"Ontario","country":"Canada","postalCode":"M8C 1F6"}},"writtenDetail":{"prescribedAsBrand":false,"productName":"Januvia","prescribedRefills":6,"writtenDate":"2022-01-11T09:25:32.602Z","ndc10":"0006-0112-31","deaCode":"C0","prescribedQuantity":{"value":30,"unit":"ML"}},"directions":{"text":"take one tablet every morning","foreignText":null},"rarStatus":"Pending","refillAuthorizationRequestMode":"Internal"}'
+  Log.Message("createRx RequestBodyData:"+ requestBody);
+  
+  //Create the aqHttpRequest object
+   aqHttpRequest = aqHttp.CreateRequest("PUT", requestUrl);
+  
+  //Specify the Content-Type header value
+  aqHttpRequest.SetHeader("Content-Type", "application/json");
+ 
+  // Send the request, create the aqHttpResponse object
+  responseBody = aqHttpRequest.Send(requestBody);
+  Log.Message("createRx status code:"+responseBody.StatusCode);
+  createPatient.verifyStatusCode(responseBody, 201);  
+ }
+
+//Get newly created patient
+  function getRx(){
+  //Request url for get rx.
+   requestUrl = Project.Variables.patientBaseUrl + patientId +"/rx";
+   aqHttpRequest = aqHttp.CreateGetRequest(requestUrl);
+   
+   //Send the request, get an aqHttpResponse object
+   responseBody = aqHttpRequest.Send();
+   Log.Message("getRx response is: "+responseBody);
+   
+   //Verify status code.
+   createPatient.verifyStatusCode(responseBody, 200);
+  }
+  
+  //Store Rx id.
+  function storeRxDetails(){
+    jsonResponseBody = JSON.parse(responseBody);
+    rxId= jsonResponseBody[0].prescriptionId;
+    Log.Message("getRx prescriptionId from response: "+rxId);
+    Project.Variables.rxId= rxId;
+    Log.Message("getRx rx id is"+Project.Variables.rxId);
+  }
+
+  
